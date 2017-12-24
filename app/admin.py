db2d6886d0a36b806b94695dd2379d142d198693
parksangmoon <cholsoo22001@gmail.com>
2017-12-24 15:00:50 +0900

    redirect URL 설정. login logout 설정

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from app.models import Profile
+# Register your models here.
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'email_confirmed'
+    )
+
+admin.site.register(Profile, ProfileAdmin)
 
 # Register your models here.
--- doyouremember/urls.py
+++ doyouremember/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from app import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', views.sign_up, name='sign_up'),
+    path('login/', auth_views.login,{'template_name':'login.html'}, name='login'),
+    path('logout/', auth_views.logout, name='logout'),
     path('account_activation_sent/', views.account_activation_sent,
          name='account_activation_sent'),
     re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})$',
